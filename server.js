92b89e29f83607ead7ef84921d1eca152e0210f4
celin <creptonium@gmail.com>
2018-07-27 08:49:51 +1000

    rognition endpoint

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 5000;
 
+rekognitionResponse = {
+    "ageRange": {
+        "low": 23,
+        "high": 38
+    },
+    "gender": {
+        "value": "Male",
+        "confidence": 99.9922226
+    },
+    "confidence": 99.98948,
+    "estimatedAge": 30
+}
+
 app.get('/api/hello', (req, res) => {
     res.send({ price: 764 });
 });
@@ -26,5 +39,12 @@ app.post('/api/hello/post', function(req, res) {
 app.get('/api/hello/query', function(req, res){
     res.send({ price: 123000 });
 });
+
+app.post('/rekognition/detect', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send(rekognitionResponse);
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
