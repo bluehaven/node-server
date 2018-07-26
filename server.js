868009402a9da1a6caa5f85a1779d37ee489cb08
celin <creptonium@gmail.com>
2018-07-27 03:42:02 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 5000;
+
+app.get('/api/hello', (req, res) => {
+    res.send({ price: 764 });
+});
+
+app.post('/api/hello/post', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send({ price: 123 });
+});
+
+app.get('/api/hello/query', function(req, res){
+    res.send({ price: 123000 });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
